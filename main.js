8ee609dfd92d4ebe39d8085f8728c07eb7ddc569
GustavoG0etten <gustavogoetten87@gmail.com>
2024-04-07 14:34:35 -0300

    feat: :alien: Comecando a criar a carta de apresentacao

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 
-const myIcon = document.getElementById('.botao-linguagem');
+// Scroll Reveal
 
-myIcon.addEventListener('mouseover', function() {
-    myIcon.textContent = 'HTML5';
-    myIcon.classList.add('javascript');
-});
+window.revelar = ScrollReveal({reset:true});
 
-myIcon.addEventListener('mouseout', function() {
-    myIcon.textContent = <i class="fa-brands fa-html5"></i>;
-    myIcon.classList.remove('javascript');
-});
+revelar.reveal('.efeito-texto-topo',{
+    duration: 2000,
+    distance: '90px'
+})
+
+
+
+
+
+
+// const myIcon = document.getElementById('.botao-linguagem');
+
+// myIcon.addEventListener('mouseover', function() {
+//     myIcon.textContent = 'HTML5';
+//     myIcon.classList.add('javascript');
+// });
+
+// myIcon.addEventListener('mouseout', function() {
+//     myIcon.textContent = <i class="fa-brands fa-html5"></i>;
+//     myIcon.classList.remove('javascript');
+// });
