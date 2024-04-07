068eac4806bebc4052d9b30836e0c62d2356ee10
GustavoG0etten <gustavogoetten87@gmail.com>
2024-04-07 08:34:54 -0300

    feat: :alien: Criando um carrossel animado e efeito de paralax

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 
-window.onscroll = function () { scrollFunction() };
+const myIcon = document.getElementById('.botao-linguagem');
 
-function scrollFunction() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        // Quando ele detectar que tem mais que 80px ele automaticamente adiciona a 
-        // classe scroll ai header
-        document.getElementById("navbar").classList.remove("scroll");
-    } else {
-        // Aqui quando o scroll for menor que os 80px mencionados acima ele remove 
-        // a classe do header
-        document.getElementById("navbar").classList.remove("scroll");
-    }
-}
+myIcon.addEventListener('mouseover', function() {
+    myIcon.textContent = 'HTML5';
+    myIcon.classList.add('javascript');
+});
+
+myIcon.addEventListener('mouseout', function() {
+    myIcon.textContent = <i class="fa-brands fa-html5"></i>;
+    myIcon.classList.remove('javascript');
+});
