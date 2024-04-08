30adfa9d8b9fa4dfaed044373243c8a14b2600d8
GustavoG0etten <gustavogoetten87@gmail.com>
2024-04-08 18:20:40 -0300

    feat: :alien: Criei o design da carta de apresentacao

--- main.js
+++ main.js
@@ -11,16 +11,3 @@ revelar.reveal('.efeito-texto-topo',{
 
 
 
-
-
-// const myIcon = document.getElementById('.botao-linguagem');
-
-// myIcon.addEventListener('mouseover', function() {
-//     myIcon.textContent = 'HTML5';
-//     myIcon.classList.add('javascript');
-// });
-
-// myIcon.addEventListener('mouseout', function() {
-//     myIcon.textContent = <i class="fa-brands fa-html5"></i>;
-//     myIcon.classList.remove('javascript');
-// });
